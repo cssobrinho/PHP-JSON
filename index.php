326fc17e131d2e71ae3eeb14f740c2c370dbc7f5
cssobrinho <cssobrinho2014@gmail.com>
2020-05-04 14:00:16 -0300

    Servidor Php - Json aula 2 Canal DevPleno

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
 # servidor Php DevPleno Json
+if(!array_key_exists('path', $_GET))
+{
+	echo 'Error. Path missing.';
+	exit;
+}
 
 $path = explode('/', $_GET['path']);
+
+if(count($path)==0  || $path[0]=="")
+{
+	echo 'Error. Path missing.';
+	exit;
+}
+
+$param1= "";
+if(count($path)>1)
+{
+	$param1 = $path[1];
+}
+
 $contents = file_get_contents('db.json');
 
 $json = json_decode($contents, true);
@@ -14,7 +32,29 @@ $body =file_get_contents('php://input');
 if($method === 'GET')
 {
 	if($json[$path[0]]){
-		echo json_encode($json[$path[0]]);
+		if($param1=="")
+		{
+			echo json_encode($json[$path[0]]);
+		}else
+		{
+			$encontrado = -1;
+			foreach($json[$path[0]] as $key => $obj)
+			{
+				if($obj['id'] == $param1)
+				{
+					$encontrado = $key;
+					break;
+				}
+			}
+			if($encontrado >= 0)
+			{
+				echo json_encode($json[$path[0]][$encontrado]);
+			}else
+			{
+				echo 'Error.';
+			}
+		}
+		
 	}else
 	{
 		echo '[]';
